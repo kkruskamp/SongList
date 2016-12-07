6ab6a50a81f6ad03ba98d18a0db5d684af692f30
Kyle <kkruskamp@mail.csuchico.edu>
2016-12-07 15:46:07 -0800

    first

--- songlist.js
+++ songlist.js
@@ -9,13 +9,17 @@ function Song(title,artist,album,year)
   this.artist = artist;
   this.album = album;
   this.year = year;
+
   this.toString = function(){
     return title + " by " + artist;
   }
+
   this.releaseInfo = function(){
     return "Released on " + album + " in " + year;
   };
+
   this.listened = 0;
+
   this.play = function(){
     listened++;
   };
@@ -29,18 +33,19 @@ function loadList()
   allSongs.push(new Song("Get Lucky","Daft Punk","Random Access Memories",2013));
   allSongs.push(new Song("Inevitable","Shakira","Dónde Están los Ladrones?",1998));
   allSongs.push(new Song("Wagon Wheel","Old Crow Medicine Show","Old Crow Medicine Show",2004));
+  displaySong();
 }
 
 //Progresses to the next song to the list, or if the end of the list has been
 //reached, then loop back to the beginning
 function nextSong()
 {
-  current = (current+1)%allSongs.length;
+  current = (current + 1) % allSongs.length;
   displaySong();
 }
 
 //Show song's information in the "nowPlaying" span
 function displaySong()
 {
-  document.getElementById("nowPlaying").innerHTML = allSongs[current].toString();
+  document.getElementById("nowPlaying").innerHTML = allSongs[current].toString() + " " + allSongs[current].releaseInfo();
 }
